2322ca7309ecbbe50bed3568aea45fc80d51cf00
2damin <leesak2003@naver.com>
2022-03-19 16:19:37 +0900

    rename the layers depending on pretrained_weights

--- model/Darknet53.py
+++ model/Darknet53.py
@@ -1,27 +1,23 @@
 import torch
 from torch import nn
 
-class ConvBlock(nn.Module):
-    def __init__(self, in_c, out_c, kernel, pad, stride):
-        self.conv = nn.Conv2d(in_c, out_c, kernel, pad, stride, bias=False)
-        self.bn = nn.Batchnorm2d(out_c)
-        self.act = nn.LeakyReLU()
-        self.modules = nn.Sequential([self.conv,self.bn,self.act])
-    
-    def forward(self, x):
-        x = self.modules(x)
-        return x
+def ConvBlock(in_c, out_c, kernel, pad, stride):
+        conv = nn.Conv2d(in_c, out_c, kernel, pad, stride, bias=False)
+        bn = nn.BatchNorm2d(out_c)
+        act = nn.LeakyReLU()
+        return nn.Sequential(*[conv,bn,act])
 
 class ResidulBlock(nn.Module):
     def __init__(self, in_c):
+        super().__init__()
         mid_c = in_c//2
-        self.conv0 = ConvBlock(in_c, mid_c, 1, 0, 1)
-        self.conv1 = ConvBlock(mid_c, in_c, 3, 1, 1)
+        self.layer1 = ConvBlock(in_c, mid_c, 1, 0, 1)
+        self.layer2 = ConvBlock(mid_c, in_c, 3, 1, 1)
     
     def forward(self, x):
         residual = x
-        x = self.conv0(x)
-        x = self.conv1(x)
+        x = self.layer1(x)
+        x = self.layer2(x)
         x += residual
         return x
     
@@ -35,19 +31,19 @@ class DarkNet53(nn.Module):
         self.in_channel = in_channel
         self.is_train = is_train
         
-        self.conv0 = ConvBlock(in_channel, 32, 3, 1, 1)
-        self.conv1 = ConvBlock(32, 64, 3, 1, 2)
-        self.resblock2 = ResidulBlock(64)
+        self.conv1 = ConvBlock(in_channel, 32, 3, 1, 1)
+        self.conv2 = ConvBlock(32, 64, 3, 1, 2)
+        self.residual_block1 = self.make_layer(ResidulBlock, 64, 1)
         self.conv3 = ConvBlock(64, 128, 3, 1, 2)
-        self.resblock4 = self.make_layer(ResidulBlock, 128, 2)
-        self.conv5 = ConvBlock(128, 256, 3, 1, 2)
-        self.resblock6 = self.make_layer(ResidulBlock, 256, 8)
-        self.conv7 = ConvBlock(256, 512, 3, 1, 2)
-        self.resblock8 = self.make_layer(ResidulBlock, 512, 8)
-        self.conv9 = ConvBlock(512, 1024, 3, 1, 2)
-        self.resblock10 = self.make_layer(ResidulBlock, 1024, 4)
+        self.residual_block2 = self.make_layer(ResidulBlock, 128, 2)
+        self.conv4 = ConvBlock(128, 256, 3, 1, 2)
+        self.residual_block3 = self.make_layer(ResidulBlock, 256, 8)
+        self.conv5 = ConvBlock(256, 512, 3, 1, 2)
+        self.residual_block4 = self.make_layer(ResidulBlock, 512, 8)
+        self.conv6 = ConvBlock(512, 1024, 3, 1, 2)
+        self.residual_block5 = self.make_layer(ResidulBlock, 1024, 4)
         self.gap = nn.AdaptiveAvgPool2d((1, 1))
-        self.fc = torch.Linear(1024,self.n_classes)
+        self.fc = nn.Linear(1024,self.n_classes)
         
     def make_layer(self, block, in_channel, num_block):
         layers = []
