014cd84465c171dfff77dd490f93d662ad23018d
2damin <leesak2003@naver.com>
2022-04-08 02:22:31 +0900

    bug fix

--- model/Darknet53.py
+++ model/Darknet53.py
@@ -2,7 +2,7 @@ import torch
 from torch import nn
 
 def ConvBlock(in_c, out_c, kernel, pad, stride):
-        conv = nn.Conv2d(in_c, out_c, kernel, stride, pad, bias=False)
+        conv = nn.Conv2d(in_c, out_c, kernel, stride, pad, bias=True)
         bn = nn.BatchNorm2d(out_c)
         act = nn.LeakyReLU()
         return nn.Sequential(*[conv,bn,act])
@@ -43,7 +43,27 @@ class DarkNet53(nn.Module):
         self.conv6 = ConvBlock(512, 1024, 3, 1, 2)
         self.residual_block5 = self.make_layer(ResidulBlock, 1024, 4)
         self.gap = nn.AdaptiveAvgPool2d((1, 1))
-        self.fc = nn.Linear(1024,self.n_classes)
+        self.fc1 = nn.Linear(1024,self.n_classes)
+        self.drop = nn.Dropout(0.5)
+        self.sigmoid = nn.Sigmoid()
+        
+        self.initialize_weights()
+    
+    def initialize_weights(self):
+            # track all layers
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_uniform_(m.weight)
+
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+            elif isinstance(m, nn.Linear):
+                nn.init.kaiming_uniform_(m.weight)
+                nn.init.constant_(m.bias, 0)
         
     def make_layer(self, block, in_channel, num_block):
         layers = []
@@ -55,7 +75,7 @@ class DarkNet53(nn.Module):
 
         x = self.conv1(x)
         x = self.conv2(x)
-        x = self.residual_block1(x)
+        x = self.residual_block1(x) 
         x = self.conv3(x)
         x = self.residual_block2(x) 
         x = self.conv4(x)
@@ -66,6 +86,8 @@ class DarkNet53(nn.Module):
         x = self.residual_block5(x)
         x = self.gap(x)
         x = x.view(-1,1024)
-        x = self.fc(x)
+        x = self.drop(x)
+        x = self.fc1(x)
+        x = self.sigmoid(x)
         return x
         
