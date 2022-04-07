b8280752cff5d162cf2f2279b8b7ad19c066b98f
2damin <leesak2003@naver.com>
2022-04-08 02:22:08 +0900

    ce->bce

--- loss/loss.py
+++ loss/loss.py
@@ -3,18 +3,17 @@ from torch import nn
 import sys
 
 class BCEloss(nn.Module):
-    def __init__(self, device = torch.device('cpu')):
+    def __init__(self, device = torch.device('cpu'), weight = None):
         super(BCEloss, self).__init__()
-        self.loss = nn.CrossEntropyLoss().to(device)
-    
+        self.loss =nn.BCELoss().to(device)
+
     def forward(self, out, gt):
         loss_val = self.loss(out, gt)
         return loss_val
     
-def get_criterion(crit = "bce", device = torch.device('cpu')):
+def get_criterion(crit = "bce", device = torch.device('cpu'),  weight = None):
     if crit == "bce":
-        return BCEloss(device = device)
+        return BCEloss(device = device, weight = weight)
     else:
         print("unknown criterion")
         sys.exit(1)
-
